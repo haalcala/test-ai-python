58f03cd3c4f4a09748b30588e2a964304710cf54
Harold Alcala <harold.dev@gmail.com>
2021-10-18 18:05:12 +0800

    tensorflow gpu

---
+++ tensorflow-gpu/README.md
@@ -0,0 +1,1 @@
+https://medium.com/analytics-vidhya/using-tensorflow-on-windows-10-with-nvidia-rtx-3000-series-gpus-637ab2a4b163
---
+++ tensorflow-gpu/test_tensorflow.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+print(tf.test.gpu_device_name())
+print(tf.config.list_physical_devices("GPU"))
+# print(tf)
